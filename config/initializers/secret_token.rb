36a25c61bca40cf923a153220ca650f13488c8f4
Shashwat Sinha <shashwat-sinha@VIMID-SinhaS-LM.local>
2019-08-13 15:59:32 +0530

    Initial repo setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3f896ba1c4ea2ed716246fbb8c20ab0087b6db179b8f31656c672181101a2839b5ea9b5df3a371f563f2e44759becb24b41275f886a03c906feea7b52488489a'
